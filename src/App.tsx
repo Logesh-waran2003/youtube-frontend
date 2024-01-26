9c208a9322d4727149d13078fa36f377f508af1f
Logesh <Logeshwaransrini@gamil.com>
2024-01-26 22:05:53 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PageHeader from './Components/PageHeader'
+
+const App = () => {
+  return (
+<div className="max-h-screen flex flex-col">
+      <PageHeader />
+      <div className=""></div>
+    </div>  )
+}
+
+export default App
---
+++ src/Components/PageHeader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PageHeader = () => {
+  return (
+    <div className=''></div>
+  )
+}
+
+export default PageHeader
